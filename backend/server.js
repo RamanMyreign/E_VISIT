ca0ca78b1d924f6f168093d82f5841de16d750f4
RamanMyreign <raman.shukla_cs18@gla.ac.in>
2021-08-23 02:09:38 +0530

    atlas link updated

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,12 @@ const mongoose = require('mongoose');
 // 'mongodb+srv://raman:Raman@123@cluster0.mgk05.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT ||  5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/E_VISIT', {useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology:true, useFindAndModify:false}
+mongoose.connect('mongodb+srv://raman:Raman@123@cluster0.mgk05.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology:true, useFindAndModify:false}
 ).then(() => {console.log("MongoDB database connection established successfully");})
 .catch((err) => console.log(err));
 const connection = mongoose.connection;
@@ -24,6 +24,6 @@ const newuserRouter = require('./routes/newUser');
 app.use('/doctorList', doctorListRouter);
 app.use('/newUser', newuserRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
